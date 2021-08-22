dcc43e27656d297da288b8b5ac74a3dba833d15c
jmeaster30 <jhneasterday09@gmail.com>
2021-08-21 22:22:53 -0400

    Created test file

--- git-merge-polite.py
+++ git-merge-polite.py
@@ -1,8 +1,13 @@
 import sys.argv
+import os
 
 ancestor = sys.argv[1]
 current = sys.argv[2]
 other = sys.argv[3]
 
-#this just takes your chamges
-
+print("Ancestor\n")
+print(ancestor)
+print("\nCurrent\n")
+print(current)
+print("\nOther\n")
+print(other)
